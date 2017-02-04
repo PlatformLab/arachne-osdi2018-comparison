3265ac1c590ef340b382692c335edbfe7b5cbca7
Henry Qin <hq6@stanford.edu>
2017-02-03 23:11:05 -0800

    Updated RunGoThread to append a starting event

--- RunGoThreadCreate.sh
+++ RunGoThreadCreate.sh
@@ -3,6 +3,6 @@
 for i in 2 3 4; do
     echo "GOMAXPROCS = $i"
     GOMAXPROCS=$i ./GoThreadCreate > GoThreadCreate.log
-    ttsum.py GoThreadCreate.log
+    ttsum.py -f Before GoThreadCreate.log
     echo
 done
