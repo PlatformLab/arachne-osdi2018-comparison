b1f496d90633a6e07ff533b23b642c4f2d96ecf0
Henry Qin <hq6@stanford.edu>
2017-04-06 23:47:42 -0700

    Adding std condition variable wakeup and some data

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-BINS=ThreadCreationTest ThreadYieldTest
+BINS=ThreadCreationTest ThreadYieldTest StdThreadCVWakeup
 GO_BINS=GoThreadCreate GoThreadYield
 
-CXXFLAGS=-std=c++11 -O3 -Wall -Werror -Wformat=2 -Wextra -Wwrite-strings -Wno-unused-parameter -Wmissing-format-attribute -Wno-non-template-friend -Woverloaded-virtual -Wcast-qual -Wcast-align -Wconversion -fomit-frame-pointer
+CXXFLAGS=-std=c++11 -O3 -g -Wall -Werror -Wformat=2 -Wextra -Wwrite-strings -Wno-unused-parameter -Wmissing-format-attribute -Wno-non-template-friend -Woverloaded-virtual -Wcast-qual -Wcast-align -Wconversion -fomit-frame-pointer
 LIBS=-I../Arachne  -I../PerfUtils ../PerfUtils/libPerfUtils.a   -pthread
 
 
---
+++ StdThreadCVWakeup.cc
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <vector>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+#include "Cycles.h"
+#include "Util.h"
+#include "Stats.h"
+
+#define NUM_SAMPLES 1000000
+
+using PerfUtils::Cycles;
+std::atomic<uint64_t> arrayIndex;
+uint64_t latencies[NUM_SAMPLES];
+
+std::atomic<uint64_t> beforeNotify;
+
+std::condition_variable productIsReady;
+std::mutex mutex;
+
+std::atomic<bool> producerHasStarted;
+
+void producer() {
+    producerHasStarted = true;
+	for (int i = 0; i < NUM_SAMPLES; i++) {
+        while (producerHasStarted);
+        producerHasStarted = true;
+        mutex.lock();
+        beforeNotify = Cycles::rdtsc();
+	    productIsReady.notify_one();
+        mutex.unlock();
+	}
+}
+
+void consumer() {
+    std::unique_lock<std::mutex> guard(mutex);
+	for (int i = 0; i < NUM_SAMPLES; i++) {
+        producerHasStarted = false;
+        productIsReady.wait(guard);
+        latencies[arrayIndex++] = Cycles::rdtsc() - beforeNotify;
+	}
+}
+
+int main(int argc, const char** argv){
+    // Add some work
+    std::thread producerThread(producer);
+    while(!producerHasStarted);
+    std::thread consumerThread(consumer);
+    producerThread.join();
+    consumerThread.join();
+
+    if (arrayIndex != NUM_SAMPLES) abort();
+    printStatistics("Condition Variable Wakeup", latencies, NUM_SAMPLES, "data");
+    return 0;
+}
