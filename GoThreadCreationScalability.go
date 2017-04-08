93b87127b26f2fe67777e06edf2be1181d71c5be
Henry Qin <hq6@stanford.edu>
2017-04-07 22:11:51 -0700

    Changing Golang to use a timer check instead of a shutdown flag that gets indefinitely delayed

--- GoThreadCreationScalability.go
+++ GoThreadCreationScalability.go
@@ -8,14 +8,14 @@ import "strconv"
 import "sync/atomic"
 
 const CORE_OCCUPANCY = 3
-var shutdown bool
+var stopTime time.Time
 var globalCount uint64
 
 // Artifical goroutine join
 var done chan bool
 
 func creator(counter uint64) {
-    if !shutdown {
+    if time.Now().Before(stopTime) {
         go creator(counter + 1)
     } else {
        atomic.AddUint64(&globalCount, counter)
@@ -37,20 +37,17 @@ func main() {
     runtime.GOMAXPROCS(numCores)
 
     startTime := time.Now()
+    stopTime = startTime.Add(time.Duration(numSeconds) * time.Second)
     for i := 0; i < numCores * CORE_OCCUPANCY; i++ {
         go creator(1)
     }
-
-    // When there are continuous creation of Goroutines, this sleep doesn't
-    // wake up for more than 30 seconds despite being instructed to sleep only
-    // 1 seconds.
-    time.Sleep(time.Duration(numSeconds) * time.Second)
-    shutdown = true
     // wait for all extant goroutines to finish
     for i := 0; i < numCores * CORE_OCCUPANCY; i++ {
         <-done
     }
     duration := time.Since(startTime)
+    fmt.Printf("Total Thread Creations: %v\n", globalCount)
+    fmt.Printf("Total Time in Nanoseconds: %v\n", duration.Nanoseconds())
     fmt.Printf("Thread Creations Per Second: %f\n",
         float64(globalCount) / duration.Seconds())
 }
