0bb8741e20fd89b0ecd2f86c045f3c213b3bf38b
Henry Qin <hq6@stanford.edu>
2017-09-06 12:03:51 -0700

    Final form of comparison tests used in actual paper

--- RunThreadScalability.sh
+++ RunThreadScalability.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for i in {1..7}; do ./GoThreadCreationScalability $i 5; done
-for i in {1..7}; do ./ThreadCreationScalability.sh $i 5; done
+# for i in {1..15}; do ./GoThreadCreationScalability $i 5; done
+for i in {1..15}; do ./ThreadCreationScalability.sh $i 5; done
