13f2f0aa5ca21fa7968f70f581545b51a87255d0
Henry Qin <hq6@cs.stanford.edu>
2018-04-25 01:18:08 -0600

    Fix std::thread creation scalability benchmark to work under new directory structure on m510

--- scripts/RunThreadScalability.sh
+++ scripts/RunThreadScalability.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # for i in {1..15}; do ./GoThreadCreationScalability $i 5; done
-for i in {1..15}; do ./ThreadCreationScalability.sh $i 5; done
+
+# Give CPU sets a rest in between runs
+for i in {1..15}; do scripts/ThreadCreationScalability.sh $i 5 ; sleep 1 ; done
--- scripts/ThreadCreationScalability.sh
+++ scripts/ThreadCreationScalability.sh
@@ -5,16 +5,6 @@ if [ "$#" -ne 2 ]; then
     exit
 fi
 
-if [ "$1" -le 7 ]; then
-    sudo env "PATH=$PATH" TakeCores.sh $(seq 1 $1 | paste -s -d, /dev/stdin) 0 $$
-else
-    i=$1
-    endIndex=$((i - 8 + 16))
-    cores=$(echo -n 1-7,; seq 16 $endIndex | paste -s -d, /dev/stdin)
-    sudo env "PATH=$PATH" TakeCores.sh $cores  0 $$
-fi
-    ./ThreadCreationScalability $1 $2
-    sudo env "PATH=$PATH" ReleaseCores.sh > /dev/null
-
-# More than 7, we have discontiguous range of cores to stay on one socket.
-
+sudo env "PATH=$PATH" TakeCores.sh $(seq 1 $1 | paste -s -d, /dev/stdin) 0 $$
+bin/ThreadCreationScalability $1 $2
+sudo env "PATH=$PATH" ReleaseCores.sh > /dev/null
